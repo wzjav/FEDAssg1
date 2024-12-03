828cf41067ca0985bc1eeac51c0bc7af98e0b430
wzjav <s10269031@connect.np.edu.sg>
2024-12-03 12:42:35 +0800

    change file path to view image

--- javascript/product.js
+++ javascript/product.js
@@ -7,7 +7,7 @@ function addToCart() {
   const product = {
     name: "Hokkaido-Flavoured Strawberry Milk",
     price: 7.9,
-    image: "../Pictures/Hokkaido-Milk1.webp",
+    image: "/FED_10269031_JavierGan_Assg01/Pictures/Hokkaido-Milk1.webp",
     quantity: quantity,
   };
   
